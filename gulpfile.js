3aa7ce6ec6c306f94f653d22fe255ec8f25a3649
Savva Mikhalevski <smikhalevski@gmail.com>
2016-10-25 23:54:32 +0300

    Separated molecular and atomic builds

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('scss/index.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('css'))
+  gulp.src(['scss/atomic.scss', 'scss/molecular.scss'])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function(){
