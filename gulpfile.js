f6def803382124931437910e93cc90e4f4036843
Savva Mikhalevski <smikhalevski@gmail.com>
2016-11-02 17:03:35 +0300

    Removed molecules and normalization

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  gulp.src(['scss/atomic.scss', 'scss/molecular.scss'])
+  gulp.src('scss/index.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('css'));
 });
