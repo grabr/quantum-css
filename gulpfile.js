2537becfddbdb7c745683c874d1d277d39b5fb97
Savva Mikhalevski <smikhalevski@gmail.com>
2016-11-23 00:03:29 +0300

    Travis CI integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var postcssImport = require('postcss-import');
+var cssnano = require('cssnano');
 
 gulp.task('sass', function() {
   gulp.src('scss/index.scss')
       .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([
+        postcssImport(),
+        cssnano()
+      ]))
       .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch('scss/**/*.scss', ['sass']);
 });
