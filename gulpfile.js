f6cb0f695ffc6c3c7ce96fbb2d6fc0e90bb8519a
Savva Mikhalevski <smikhalevski@gmail.com>
2018-04-08 05:44:51 +0300

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var postcssImport = require('postcss-import');
-var cssnano = require('cssnano');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const postcssImport = require('postcss-import');
+const postcssMergeRules = require('postcss-merge-rules');
+const cssnano = require('cssnano');
+const mqpacker = require('css-mqpacker');
 
 gulp.task('sass', function() {
-  gulp.src('scss/index.scss')
+  gulp.src('scss/index2.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(postcss([
-        postcssImport(),
-        cssnano()
+        mqpacker(),
+        postcssMergeRules(),
+        cssnano(),
       ]))
       .pipe(gulp.dest('css'));
 });
